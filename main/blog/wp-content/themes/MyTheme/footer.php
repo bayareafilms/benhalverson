cea6994624de3088168283e41378e31456531467
Ben Halverson <benhalverson33@gmail.com>
2014-06-17 16:23:44 -0700

    updated footer and index

--- main/blog/wp-content/themes/MyTheme/footer.php
+++ main/blog/wp-content/themes/MyTheme/footer.php
@@ -7,19 +7,19 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="benhalverson.me/assets/js/jquery.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-transition.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-alert.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-modal.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-dropdown.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-scrollspy.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-tab.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-tooltip.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-popover.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-button.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-collapse.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-carousel.js"></script>
-    <script src="benhalverson.me/assets/js/bootstrap-typeahead.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/jquery.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-transition.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-alert.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-modal.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-dropdown.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-tab.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-tooltip.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-popover.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-button.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-collapse.js"></script>
+    <script src="http://www.benhalverson.me/assets/js/bootstrap-carousel.js"></script>
+ //   <script src="http://www.benhalverson.me/assets/js/bootstrap-typeahead.js"></script>
 
   </body>
 </html>
